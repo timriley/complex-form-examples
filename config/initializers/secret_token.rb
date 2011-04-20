fca6a0cd92410abf389b33a612ac15532bb25a05
Brian Buchalter <bal711@gmail.com>
2011-04-20 00:02:06 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ComplexFormExamples::Application.config.secret_token = '37048791e681985e42df370ed8781b0ce4e3d6d67562a0a5dad327a320b329fbeee8a953e77f91fd0fb8b25854884d50eb6c5c2a249cd5cae636508fd03954c4'
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,15 @@
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
 $(function() {
   $('form a.add_child').live('click', function() {
     // Setup
     var assoc   = $(this).attr('data-association');           // Name of child
     var content = $('#' + assoc + '_fields_template').html(); // Fields template
-    
+
     // Make the context correct by replacing new_<parents> with the generated ID
     // of each of the parent objects
     var context = ($(this).parents('.fields').children('input:first').attr('name') || '').replace(new RegExp('\[[a-z]+\]$'), '');
-    
+
     // context will be something like this for a brand new form:
     // project[tasks_attributes][1255929127459][assignments_attributes][1255929128105]
     // or for an edit form:
@@ -15,7 +17,7 @@ $(function() {
     if(context) {
       var parent_names = context.match(/[a-z]+_attributes/g) || []
       var parent_ids   = context.match(/[0-9]+/g)
-      
+
       for(i = 0; i < parent_names.length; i++) {
         if(parent_ids[i]) {
           content = content.replace(
@@ -25,22 +27,22 @@ $(function() {
         }
       }
     }
-    
-    // Make a unique ID for the new child 
+
+    // Make a unique ID for the new child
     var regexp  = new RegExp('new_' + assoc, 'g');
     var new_id  = new Date().getTime();
-    content     = content.replace(regexp, new_id)
-        
+    content     = content.replace(regexp, new_id);
+
     $(this).parent().before(content);
     return false;
   });
-  
+
   $('form a.remove_child').live('click', function() {
     var hidden_field = $(this).prev('input[type=hidden]')[0];
     if(hidden_field) {
       hidden_field.value = '1';
     }
-    $(this).parent('.fields').hide();
+    $(this).parent().parent('.fields').hide();
     return false;
   });
 });
